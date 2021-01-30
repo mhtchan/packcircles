ae2c4cfe4ad5388b91956caad870cf32c05d86e9
mhtchan <78253773+mhtchan@users.noreply.github.com>
2021-01-31 00:41:16 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
-setup(name='packcircles',
-      version='0.1',
-      description='A pure Python implementation of a circle packing algorithm',
-      url='https://github.com/mhtchan/packcircles',
-      author='mhtchan',
-      author_email='mhtchan@outlook.com',
-      license='MIT',
-      packages=['packcircles'],
-      install_requires=['pyllist >= 0.3'])
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='packcircles',
+    version='0.11',
+    description='A pure Python implementation of a circle packing algorithm',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mhtchan/packcircles',
+    author='mhtchan',
+    author_email='mhtchan@outlook.com',
+    license='MIT',
+    packages=['packcircles'],
+    install_requires=['pyllist >= 0.3'])
